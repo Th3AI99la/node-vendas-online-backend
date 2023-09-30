52a2c2b3f102ed88cc1de1b7d2edc4af2feec475
“Thalles <talles.th54@gmail.com>
2023-09-29 21:21:25 -0300

    test-system-created

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+console.log('Serviço Iniciado...\n');
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(8080);
+  console.log('\nRodando na porta 8080');
+}
+
+bootstrap();
