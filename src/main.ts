df9686501525a989cc49ffc508a7d5887ad774a4
“Thalles <talles.th54@gmail.com>
2023-10-11 23:09:39 -0300

    LOGS

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 
 console.log('Serviço Iniciado...\n');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
-  console.log('\nRodando na porta 8080');
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(8080);
+    console.log('\nRodando na porta 8080');
+  } catch (error) {
+    console.error('Não foi possível iniciar o servidor. Verifique o LOG.');
+    console.error(error); // Isso exibirá o erro original no console para fins de depuração.
+  }
 }
 
 bootstrap();
